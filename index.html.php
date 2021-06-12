cec5c9f17cc2ac6a5017a60cb5bc3610e61c4714
jhasamaksh1 <jhasamaksh1@gmail.com>
2021-06-12 12:19:53 +0530

    Publishing my first website
    I hope you like it.

---
+++ index.html.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
+    
+    
+    <body>
+  
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">R.C Institute of English</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+        <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+        
+        
+        
+        </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+    
+    <div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="eduford_img/3.jpg" alt="Los Angeles" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Successful students</h3>
+        <p>We learn not to store but to Implement</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="eduford_img/library.png" alt="Chicago" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>Chicago</h3>
+        <p>Thank you, Chicago!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="eduford_img/banner2.jpg" alt="New York" width="1100" height="500">
+      <div class="carousel-caption">
+        <h3>New York</h3>
+        <p>We love the Big Apple!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+   <section class="my-5">
+    <div class="py-5">
+       <h3 class="text-center">
+        About Us
+        </h3>
+       </div>
+       <div class="container-fluid">
+           <div class="row">
+               <div class="col-lg-6 col-md-6 col-12">
+                   <img src="eduford_img/poster1.jpg" class="img-fluid aboutimg">
+           
+           </div>
+                <div class="col-lg-6 col-md-6 col-12">
+                  <h3 class="display-4">Rc Institute in Pitampura, Delhi</h3>
+                    <p class="py-3">Rc Institute in Delhi is one of the leading businesses in the Tutorials For English For Bank PO Exam. Also known for Language Classes For English, Tutorials, Language Classes For English Conversation, IELTS Tutorials, Tutorials For Ctet, Tutorials For English, Language Classes, Tutorials For DSSSB and much more. Find Address, Contact Number, Reviews & Ratings, Photos, Maps of Rc Institute, Delhi.
+
+Founded in the year 1993, Rc Institute in Pitampura, Delhi is an institute that aids in the development and refinement of one's English communication skills. This establishment was built with an aim to become one of the preferred language training institutes in the region. This organisation was set up with a view to train and develop its students with high-quality communicative English through individual attention and overall personality development. It is well-known as a centre for learning English for competitive examinations as well as for effective communication. Over the years, they have trained people countless citizens belonging to different walks of life; from students to working professionals. With years of experience in this field under their belt, they have established a repute for their quality teaching methods that help them understand the specific requirements of every learner at their institute. It is on the Jhulelal Appartment, Near Gate No-3. Undoubtedly it is one of the best language class for English in Pitampura, Delhi.</p>
+                    <a href="about.php" class="btn btn-success"> check more</a>
+           
+           </div>
+       </div>
+    </div>
+    </section>     
+        
+       <section class="my-5">
+    <div class="py-5">
+       <h3 class="text-center">
+        Our Services
+        </h3>
+       </div> 
+    <div class="container-fluid">
+           <div class="row">
+    <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="eduford_img/P1.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">ALL ABOUT WRITING</h4>
+    <p class="card-text">Be a published writer:let your pen rollic and frollic abiding by our       comprehensive imaginative pen picture.it includes proofreading,editing,copywriting,journalisim,children prose,and poetry,novel,fiction,non-fiction,article writing,competitve writing,Biographs,memoirs and family histore,Anthology. </p>
+    
+</div>
+</div>
+</div>
+               <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="eduford_img/P2.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">ALL ABOUT SPEAKING</h4>
+    <p class="card-text">Communication makes man superior to animal or speaking to readyman.Join our courses how to speak to anyone ,anytime and anywhere.Public speaking to acquire confidence,Power fluency,pitch,proficiency in Sementicism,Inflection,Impeccable Deleverance,Distinctness and precision of utterence,gesture,thought and personality,Reservoir of vocablary.How to be a amass,acrowd,a mobe,innt,winning crowd,gregarious,impeccable and invincible by the art of pithy or brevity. </p>
+    
+</div>
+</div>
+</div>
+               <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="eduford_img/P3.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">ALL NEEDS AND ALL AGES</h4>
+    <p class="card-text">Anyone including kids , children,students,Man,Woman,personnells;be it house wives,Buisenessmen,collegiates,competiton aspirants for all sorts of Jobs in our course .one-on-one and one-to-one to get into courses of grooming to Perfect.
+      Grammar of all levels,constituents language and alegant literature with spiritual and intellectual vitality.English for primary or elementary level I to VI, Funmctional for VI to X , Advanced writing and speaking skills for X - XII,B.A(Hons.) English,M.A.,Theses Writing,Ph.D.</p>
+    
+</div>
+</div>
+</div>
+               
+               
+                <div class="col-lg-4 col-md-4 col-12">
+        <div class="card" >
+  <img class="card-img-top" src="eduford_img/P4.jpg" alt="Card image">
+  <div class="card-body">
+    <h4 class="card-title">GO ABROAD OR BE A GOVERNMENT EMPLOYEE</h4>
+    <p class="card-text">FOR COMPETITIVE EXAMINATIONS LIKE:
+
+IBPS-CWE Bank PO/Clerical/Specialist Officers, RRB Officers; SBI-PO/Clerical; NABARD & IDBI Bank Executive Officers
+        
+● SSC-CGL (Tier I and II); SSC-CHSL (10+2); SSC-FCI Grade III; SSC-CPO/ SI/ASI, Income Tax, etc
+LIC/GIC/UIICO AAOs, etc
+● UPSC-CSAT, SCRA, etc; and other State Services Exams
+● Railways Grade 'D' and other technical and non-technical exams
+MAT; CMAT; CET (MBA); SNAP; BBA; BBM and other B School Admission Tests
+● NTSE; CLAT; Hotel Management, etc
+</p>
+    
+</div>
+</div>
+</div>
+</div>
+</div>
+        </section>
+    
+    <section class="my-5">
+    <div class="py-5">
+       <h3 class="text-center">
+        Our Gallery
+        </h3>
+       </div>
+    
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="eduford_img/g1.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="eduford_img/g2.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="eduford_img/g3.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="eduford_img/g4.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="eduford_img/g5.jpg" class="img-fluid pb-4">
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+            <img src="eduford_img/g6.jpg" class="img-fluid pb-4">
+        </div>
+        </div>
+    </div>
+    </section>
+    
+    <section class="my-5">
+    <div class="py-5">
+       <h3 class="text-center">
+        Contact
+        </h3>
+       </div>
+        <div class="w-50 m-auto">
+            <form action="userinfo.php" method="post">
+                <div class="form-group">
+                    <label>Username
+                    </label>
+                     <input type="text" name="user" autocomplete="off" class="form-control" > 
+                </div>
+            <div class="form-group">
+                    <label>Email-Id
+                    </label>
+                     <input type="text" name="email" autocomplete="off" class="form-control" > 
+                </div>
+                <div class="form-group">
+                    <label>Mobile
+                    </label>
+                     <input type="text" name="mobile" autocomplete="off" class="form-control" > 
+                </div>
+                <div class="form-group">
+                    <label>Comment
+                    </label>
+                     <textarea class="form-control" name="comment"></textarea> 
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+            
+        </div>
+        
+        
+    </section>
+    <footer>
+        <p class="p-3 bg-dark text-white text-center">rcdelinguist@gmail.com
+        </p>
+    </footer>
+    
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+        
+        
+        
+        
+        
+        
+        
+        
+  </body>      
+</html>
+    
